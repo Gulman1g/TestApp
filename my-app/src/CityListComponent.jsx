69be5ace1cebd94bf21853ef2e3ea09d66ad4a78
Gulman1g <Kiseluk1998@gmail.com>
2023-08-20 20:20:46 +0200

    Add CityList

--- my-app/src/CityListComponent.jsx
+++ my-app/src/CityListComponent.jsx
@@ -1,15 +1,39 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+const GetCityList = () => {
+    return (
+        <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={CityListData}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Movie" />}
+        />
+    );
+}
+const CityListData = [
+        'The Shawshank Redemption',
+        'The Godfather',
+        'The Godfather: Part II',
+        'The Dark Knight',
+        '12 Angry Men',
+        "Schindler's List",
+        'Pulp Fiction',
+        'The Lord of the Rings: The Return of the King'
+  ]
 
 const CityListComponent = () => {
     return (
         <div>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    Label
+                    Find Fly
                 </Grid>
                 <Grid item xs={12}>
-                    Buttons
+                    {GetCityList()}
                 </Grid>
                 <Grid item xs={12}>
                     DataList

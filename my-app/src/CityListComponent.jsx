cd3f53ea83abf66c93253c89db070813f1aeb212
Gulman1g <Kiseluk1998@gmail.com>
2023-08-20 20:27:52 +0200

    Add Grid and CityButton

--- my-app/src/CityListComponent.jsx
+++ my-app/src/CityListComponent.jsx
@@ -3,27 +3,40 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const GetCityList = () => {
+const RenderCityListFrom = () => {
     return (
         <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={CityListData}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Movie" />}
+            renderInput={(params) => <TextField {...params} label="City From" />}
         />
     );
 }
+
+const RenderCityListTo = () => {
+    return (
+        <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={CityListData}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="City To" />}
+        />
+    );
+}
+
 const CityListData = [
-        'The Shawshank Redemption',
-        'The Godfather',
-        'The Godfather: Part II',
-        'The Dark Knight',
-        '12 Angry Men',
-        "Schindler's List",
-        'Pulp Fiction',
-        'The Lord of the Rings: The Return of the King'
-  ]
+    'London',
+    'The Godfather',
+    'The Godfather: Part II',
+    'The Dark Knight',
+    '12 Angry Men',
+    "Schindler's List",
+    'Pulp Fiction',
+    'The Lord of the Rings: The Return of the King'
+]
 
 const CityListComponent = () => {
     return (
@@ -33,7 +46,18 @@ const CityListComponent = () => {
                     Find Fly
                 </Grid>
                 <Grid item xs={12}>
-                    {GetCityList()}
+                    <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        {RenderCityListFrom()}
+                        </Grid>
+                        <Grid item xs={4}>
+                        {RenderCityListTo()}
+                        </Grid>
+                        <Grid item xs={4}>
+                            Date
+                        </Grid>
+                        
+                    </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     DataList

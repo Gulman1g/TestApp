8c4bf0f9dde263562434c63e2136504da0eea3a3
Gulman1g <Kiseluk1998@gmail.com>
2023-08-21 19:05:29 +0200

    Add API cities

--- my-app/src/CityListComponent.jsx
+++ my-app/src/CityListComponent.jsx
@@ -1,31 +1,9 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import axios from 'axios';
 
-const RenderCityListFrom = () => {
-    return (
-        <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={CityListData}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="City From" />}
-        />
-    );
-}
-
-const RenderCityListTo = () => {
-    return (
-        <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={CityListData}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="City To" />}
-        />
-    );
-}
 
 const CityListData = [
     'London',
@@ -38,7 +16,57 @@ const CityListData = [
     'The Lord of the Rings: The Return of the King'
 ]
 
+
+
 const CityListComponent = () => {
+    const [cities, setCities] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('https://countriesnow.space/api/v0.1/countries')
+            .then(response => {
+                let data =[];
+
+                 response.data.data
+                 .map(x => x.cities)
+                 .slice(0, 5)
+                 .forEach((item, index) => {
+                    item.map(x =>data.push(x));            
+                    });;
+
+                setCities(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
+    }, []);
+
+    const RenderCityListFrom = () => {
+        return (
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={cities}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="City From" />}
+            />
+        );
+    }
+    
+    const RenderCityListTo = () => {
+        return (
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={cities}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="City To" />}
+            />
+        );
+    }
+
     return (
         <div>
             <Grid container spacing={2}>
@@ -47,16 +75,16 @@ const CityListComponent = () => {
                 </Grid>
                 <Grid item xs={12}>
                     <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        {RenderCityListFrom()}
+                        <Grid item xs={4}>
+                            {RenderCityListFrom()}
                         </Grid>
                         <Grid item xs={4}>
-                        {RenderCityListTo()}
+                            {RenderCityListTo()}
                         </Grid>
                         <Grid item xs={4}>
                             Date
                         </Grid>
-                        
+
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
